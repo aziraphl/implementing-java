1b67d02ffc84f3b29455eb0933eb56ee85afc822
aziraphl <tsha7228@gmail.com>
2025-08-22 23:02:46 +0800

    Update main.java

--- main.java
+++ main.java
@@ -1,38 +1,54 @@
 
-package nyPackage;
+package wchanges;
 
 import java.util.*;
 
-class Node implements Comparable <Node> {
-//public class main {
-//public static void main(String[] args) {
+public class Wchanges {
+   public static void main (String[]args){
+     Node start = new Node (0,0);
+     Node goal = new Node(4,4);
+
+         List<Node> path = AStarsearch.findPath(start, goal);
+
+             if (path != null){
+         System.out.println("path found");
+         for (Node n : path){
+             System.out.println ("(" + n.x + "," + n.y + ")");
+         }
+      } else {
+         System.out.println("no path found");
+         }
+     }
+     
+static class Node implements Comparable <Node> {
       public int x, y;
       public double gCost, hCost;
-      public Node parent;
+      Node parent;
       
-      public Nodessssss(intx, int y){
+      Node (intx, int y){
           this.x = x;
           this.y = y;
+          this.gCost = Double.POSITIVE_INFINITY;
     }
-    public double fCost (){
-        return gCost = hCost;
+    double fCost (){
+        return gCost + hCost;
     }
     @Override
-    public int compareTo(Node other){
+    public int compareTo(other Node){
         return Double.compare(this.fCost(), other.fCost());        
     }
     @Override
-    public boolean euals(Object obj){
-        if (!(obj istance Node))return false;
+    public boolean equals(Object obj){
+        if (!(obj istanceof Node))return false;
         Node other = (Node) obj;
         return this.x == other.x && this.y == other.y;
     }
     @Override
     public int hashCode(){
-        return Ojects.hash(x, y);
+        return Objects.hash(x, y);
     }
 }
- public class AStarSearch {
+ static class AStarSearch {
      static int [][] grid = {
          {0, 0, 0, 0, 0},
          {0, 0, 1, 0, 0},
@@ -45,8 +61,13 @@ class Node implements Comparable <Node> {
      
      static List<Node>findPath(Node start, Node goal){
          PriorityQueue<Node> openSet = new PriorityQueue<>();
-         Set <Node> closedset = new Hashset <>();
-         openSet.add(start);
+         Map<String, Node> allNodes = new HashMap<>();
+         Set<Node> closedSet = new HashSet<>();
+
+            start.gCost = 0;
+            start.hCost = Math.abs(start.x - goal.x) + Math.abs(start.y - goal.y);
+            openSet.add(start);
+            allNodes.put(start.x + "," + start.y, start);
          
          while (!openSet.isEmpty()){
              Node current = openSet.poll();
@@ -65,11 +86,12 @@ class Node implements Comparable <Node> {
              for (int [] dir : new int [][]{1,0},{-1,0},{0,1},{0,-1}}){
                  int nx = current.x + dir[0], ny = current.y + dir[1];
                  if (nx >= 0 && ny >= 0 && nx < ROWS && ny < COLS && grid [nx][ny] == 0){
-                   Node neighborr = new Node(nx. ny);
+                   Node neighborr = allNodes.getOrDefault(key, new Node(nx, ny));
+                   allNodes.put(key, neighbor);
+                  
                    if (closedSet.contains(neighbor))continue;
                    
                    double tentativeG = current Gcost + 1;
-                   boolean better  = false;
                    
                    if (!openSet.contains(neighbor)){
                        neighbor.hCost = Math.abs(nx - goal.x) + Math.abs(ny - goal.y);
@@ -77,29 +99,18 @@ class Node implements Comparable <Node> {
                    } else if { (tentativeG < neighbor.Gcost){
                        better = true ;
                    }
-                   if (better){
+                   if (tentativeG < neighbor.gCost){
                        neighbor.gCost = tentativeG;
+                       neighbor.hCost = Math.abs(nx - goal.x) + Math.abs(ny - goal.y);
                        neighbor.parent = current;
                        openSet.add(neighbor);
                    }
-                }
-            }
-            return null;
-         }
-     }
- public static void main (String[]args){
-     Node start = new Node (0,0);
-     Node goal = new Node(4,4);
-     List<Node> path = findPath(start, goal);
+              }
+        }
+}
+return null;
+      
+
      
-     if (path != null){
-         System.out.println("path found");
-         for (Node n : path){
-             System.out.println ("(" + n.x + "," + n.y + ")");
-             }
-      } else {
-         System.out.println("no path found");
-         }
-     }
- }
-        
+ 
+
